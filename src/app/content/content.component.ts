cd3d4f5c4562b6608e26feb188702983aa2d669c
Dorian Mailhe <mailhedori@eisti.eu>
2021-07-27 19:53:38 +0200

    before progress bar mainpulation

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class ContentComponent {
   title = 'angular-cv';
+
+  public constructor(){
+  }
+
+  skills : any = [
+    {
+      title_1 : { img : "/assets/img/logo-html.png", name : "DOM" },
+      title_2 : { img : "/assets/img/logo-js.png", name : "Javascript" },
+      data_1 : ["HTML", "CSS", "Bootstrap", "Material Design"],
+      data_2 : ["Javascript", "Angular", "React", "jQuery", "ExpressJS/Sequelize"]
+    },
+    {
+      title_1 : { img : "/assets/img/logo-php.png", name : "PHP/SQL" },
+      title_2 : { img : "/assets/img/logo-photoshop.png", name : "Design et CMS" },
+      data_1 : ["PHP", "Laravel", "MySQL/PostreSQL"],
+      data_2 : ["Photoshop", "Premiere Pro", "Wordpress"]
+    }
+  ]
+
 }
+
+
