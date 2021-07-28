f338d146c37316866cd886ff2cac82b96946df36
Dorian Mailhe <mailhedori@eisti.eu>
2021-07-28 18:35:36 +0200

    before skill restructuration

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-content',
   templateUrl: 'views/content.component.html',
@@ -15,16 +14,37 @@ export class ContentComponent {
     {
       title_1 : { img : "/assets/img/logo-html.png", name : "DOM" },
       title_2 : { img : "/assets/img/logo-js.png", name : "Javascript" },
-      data_1 : ["HTML", "CSS", "Bootstrap", "Material Design"],
-      data_2 : ["Javascript", "Angular", "React", "jQuery", "ExpressJS/Sequelize"]
+      data_1 : [
+        { lang : "HTML", skill : "90%"}, 
+        { lang : "CSS", skill : "85%"}, 
+        { lang : "Bootstrap", skill : "80%"}, 
+        { lang : "Material Design", skill : "60%"}
+      ],
+      data_2 : [
+        { lang : "Javascript", skill : "80%"}, 
+        { lang : "Angular", skill : "70%"}, 
+        { lang : "React", skill : "50%"}, 
+        { lang : "jQuery", skill : "60%"}, 
+        { lang : "ExpressJS/Sequelize", skill : "65%"}
+      ]
     },
     {
       title_1 : { img : "/assets/img/logo-php.png", name : "PHP/SQL" },
       title_2 : { img : "/assets/img/logo-photoshop.png", name : "Design et CMS" },
-      data_1 : ["PHP", "Laravel", "MySQL/PostreSQL"],
-      data_2 : ["Photoshop", "Premiere Pro", "Wordpress"]
+      data_1 : [
+        { lang : "PHP", skill : "80%"}, 
+        { lang : "Laravel", skill : "70%"}, 
+        { lang : "MySQL/PostreSQL", skill : "90%"}
+      ],
+      data_2 : [
+        { lang : "Photoshop", skill : "90%"}, 
+        { lang : "Premiere Pro", skill : "50%"}, 
+        { lang : "Wordpress", skill : "85%"}
+      ]
     }
-  ]
+  ];
+
+  
 
 }
 
